a346771d65afe08521cb6f7b6aed31019374b9aa
Michael <m.poddorogin@mail.com>
2022-06-15 12:15:13 +0300

    forgot smth

--- test.js
+++ test.js
@@ -15,9 +15,15 @@ let requiredRange1 = [null, 200];
 let requiredRange2 = [100, 350];
 let requiredRange3 = [200, null];
 
-let filterArray = (targetArray, priceRange) => targetArray.filter(cours =>
-    Math.min(...cours.prices) >= Math.min(...priceRange) && Math.max(...cours.prices) <= Math.max(...priceRange))
-
+let filterArray = (targetArray, priceRange) => targetArray.filter(cours => {
+    if (priceRange[0] === null) {
+        return Math.max(...cours.prices) <= priceRange[1]
+    }
+    if (priceRange[1] === null) {
+        return (Math.min(...cours.prices) ? Math.min(...cours.prices) : Math.max(...cours.prices)) >= priceRange[0]
+    }
+    return Math.min(...cours.prices) >= priceRange[0] && Math.max(...cours.prices) <= priceRange[1]
+})
 let sortedArray = (targetArray,priceRange) => filterArray(targetArray, priceRange).sort((a,b) => {
     if (Math.min(...b.prices) < Math.min(...a.prices)) {
         return -1
